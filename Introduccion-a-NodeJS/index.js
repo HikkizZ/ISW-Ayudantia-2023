2de9324b4f48f1cff21cc82424074ddb01fd0de9
HikkizZ <fmiranda.reb@gmail.com>
2024-09-03 12:42:33 -0400

    RM index.js

--- Introduccion-a-NodeJS/index.js
+++
@@ -1,7 +0,0 @@
-// Función simple que recibe un nombre y lo imprime en consola
-function Saluda(nombre) {
-    console.log(`Hola ${nombre}`);
-}
-
-// Llamamos a la función Saluda con el argumento 'Pipe!'
-Saluda('Pipe!');
