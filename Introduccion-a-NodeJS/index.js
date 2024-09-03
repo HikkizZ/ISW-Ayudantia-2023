ce2276fe689f0cee26403187601b206d093ebaf4
HikkizZ <fmiranda.reb@gmail.com>
2024-09-03 12:51:39 -0400

    ADD index.js

---
+++ Introduccion-a-NodeJS/index.js
@@ -0,0 +1,7 @@
+// Función que recibe un nombre y retorna un saludo
+function Saludar(name) {
+  return `Hola ${name}!`;
+}
+
+// Se llama a la función Saludar con el argumento "Mundo"
+console.log(Saludar("Mundo"));
